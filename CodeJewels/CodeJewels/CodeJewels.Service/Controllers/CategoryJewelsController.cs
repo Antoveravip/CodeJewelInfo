90d192c644034ab54ffcd303fb0bb7766d554dc5
Antoveravip <petar@petarivanov.com>
2013-08-18 21:21:13 +0300

    Controllers
    Add controlls

---
+++ CodeJewels/CodeJewels/CodeJewels.Service/Controllers/CategoryJewelsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CodeJewels.Data;
+using CodeJewels.Models;
+
+namespace CodeJewels.Service.Controllers
+{
+    public class CategoryJewelsController : ApiController
+    {
+        CodeJewelsContext dbContext;
+
+        public CategoryJewelsController(CodeJewelsContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET api/categoryjewels
+        public IEnumerable<CodeJewelCategory> Get()
+        {
+            return this.dbContext.CodeJewelCategory.AsEnumerable();
+        }
+
+        // GET api/categoryjewels/5
+        public CodeJewelCategory Get(int id)
+        {
+            return this.dbContext.CodeJewelCategory.Find(id);
+        }
+
+        // POST api/categoryjewels
+        public CodeJewelCategory Post(CodeJewelCategory value)
+        {
+            var added = this.dbContext.CodeJewelCategory.Add(value);
+            this.dbContext.SaveChanges();
+
+            return added;
+        }
+
+        // PUT api/categoryjewels/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/categoryjewels/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CodeJewels/CodeJewels/CodeJewels.Service/Resolvers/DbDependencyResolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CodeJewels.Data;
+using System.Web.Http.Dependencies;
+using CodeJewels.Service.Controllers;
+
+namespace CodeJewels.Service.Resolvers
+{
+    public class DbDependencyResolver : IDependencyResolver
+    {
+        private static CodeJewelsContext dbContext = new CodeJewelsContext();
+
+        public IDependencyScope BeginScope()
+        {
+            return this;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(CategoryJewelsController))
+            {
+                return new CategoryJewelsController(dbContext);
+            }
+            else if (serviceType == typeof(CodeJewelsController))
+            {
+                return new CodeJewelsController(dbContext);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return new List<object>();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
